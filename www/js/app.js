3c322dbf1131d27b8142d0dcfd3ea359bedba347
Yan Mello <yan_mvd@hotmail.com>
2019-04-16 21:27:12 -0300

    primeiro commit

---
+++ www/js/app.js
@@ -0,0 +1,33 @@
+
+$('.collection').on('click', '.collection-item', function () {
+    var $badge = $('.badge', this);
+    var nomeProduto = this.firstChild.textContent; 
+    Materialize.toast(nomeProduto + '	adicionado', 1000);
+    //correção aplicada por mim para não dar erro na hora de fazer parse em um text vazio
+    if ($badge.text() === "") { $badge.text('0'); }
+
+    if ($badge.length === 0) {
+        $badge = $('<span	class="badge	brown-text"></span>').appendTo(this);
+    }
+    $badge.text(parseInt($badge.text()) + 1);
+});
+$('.modal-trigger').leanModal();
+$('#confirmar').on('click', function () {
+    var texto = "";
+    $('.badge').parent().each(function () {
+        //correção para evitar trazer os valores zerados ou em branco na lista
+        if (this.lastChild.textContent) {
+            texto += this.firstChild.textContent + ':	';
+            //corrigido para gerar uma quebra de linha para cada item da lista
+            texto += this.lastChild.textContent + ',\n	 ';
+        }
+    });
+    $('#resumo').empty().text(texto);
+});
+$('.collection').on('click', '.badge', function () {
+    var $parent = this.parentNode;
+    $(this).remove();
+    //correção para evitar erro ao remover esse nó, não contava pois o nó badge não existia
+    $('<span	class="badge	brown-text"></span>').appendTo($parent);     
+    return false;
+})
