708ef7f252abe911075d01957fd9eb71e9573019
Yan Mello <yan_mvd@hotmail.com>
2019-05-14 19:49:52 -0300

    Versão Final GarçonApp

--- www/js/app.js
+++ www/js/app.js
@@ -38,4 +38,4 @@ $('.acao-limpar').on('click', function () {
         if (this.lastChild.textContent !== "") this.lastChild.textContent = "";
     });
 });
-$('.scan-qrcode').on('click',function(){cordova.plugins.barcodeScanner.scan(function(resultado){if(resultado.text){Materialize.toast('Mesa '+ resultado.text, 2000);$('#numero-mesa').val(resultado.text);}},function(error){Materialize.toast('Erro: '+error,3000,'red-text');});});
+$('.scan-qrcode').on('click', function () { cordova.plugins.barcodeScanner.scan(function (resultado) { if (resultado.text) { Materialize.toast('Mesa	' + resultado.text, 2000); $('#numero-mesa').val(resultado.text); } }, function (error) { Materialize.toast('Erro:	' + error, 3000, 'red-text'); }); });
