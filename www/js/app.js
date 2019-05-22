bad094013900c3df1961ef436ebdf62d3e104639
Yan Mello <yan_mvd@hotmail.com>
2019-05-21 22:11:56 -0300

    comunicando com o server

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,20 @@
-
+$('.acao-finalizar').on('click', function () {
+    $.ajax({
+        url: 'http://cozinhapp.sergiolopes.org/novopedido',
+        data: {
+            mesa: $('#numero-mesa').val(),
+            pedido: $('#resumo').text()
+        },
+        error: function(erro){
+            Materialize.toast(erro.responseText,3000,'red-text');
+        },
+        success: function(dados){
+            Materialize.toast(dados,2000);
+            $('#numero-mesa').val('');
+            $('.badge').remove();
+        }
+    });
+});
 $('.collection').on('click', '.collection-item', function () {
     var $badge = $('.badge', this);
     var nomeProduto = this.firstChild.textContent;
